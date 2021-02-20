4f0135560f9dcdb22d557fa8200a551c66343750
hemmermann <hermann.jirka15@gmail.com>
2021-02-20 14:05:12 +0100

    add project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import './App.css';
-import Dashboard from './components/Dashboard';
-import Header from './components/layout/Header';
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
-  );
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
+import { render } from "@testing-library/react";
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
